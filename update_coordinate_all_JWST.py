915e2b0c2e697515ca4f28cdf5af1fd7c9b41e39
Mayankattifr <mayankn1154@gmail.com>
2024-05-03 12:17:28 +0800

    Update update_coordinate_all_JWST.py
    A code to update the position of all fits files in a directory

--- update_coordinate_all_JWST.py
+++ update_coordinate_all_JWST.py
@@ -1,56 +1,8 @@
-import time
-import numpy as np
-import scipy
-
 
 from astropy.io import fits
 from astropy import wcs
-import astropy.units as u
-from astropy.stats import sigma_clip
-from astropy.utils.data import download_file
-from spectral_cube import SpectralCube
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-
-import regions 
-import warnings
-warnings.filterwarnings("ignore", category=FutureWarning)
 
 from astropy.io import fits
-
-plt.rcParams['font.family'] = 'serif'
-plt.rcParams['font.serif'] = 'Ubuntu'
-plt.rcParams['font.monospace'] = 'Ubuntu Mono'
-plt.rcParams['font.size'] = 35
-plt.rcParams['axes.labelsize'] = 45
-plt.rcParams['xtick.labelsize'] = 45
-plt.rcParams['ytick.labelsize'] =45
-plt.rcParams['legend.fontsize'] = 45
-plt.rcParams['figure.titlesize'] = 24
-plt.rcParams['axes.labelweight']='bold'
-
-
-
-plt.rcParams['axes.linewidth'] = 5
-plt.rcParams['xtick.major.size'] = 15
-plt.rcParams['xtick.minor.size'] = 5
-plt.rcParams['ytick.major.size'] = 15
-plt.rcParams['ytick.minor.size'] = 5
-plt.rcParams['xtick.major.width'] = 3
-plt.rcParams['ytick.major.width'] = 3
-plt.rcParams['xtick.minor.width'] = 1.5
-plt.rcParams['ytick.minor.width'] = 1.5
-
-plt.rcParams['xtick.minor.visible'] = True
-plt.rcParams['ytick.minor.visible'] = True
-
-
-plt.rcParams['xtick.direction'] = 'in'
-plt.rcParams['ytick.direction'] = 'in'
-plt.rcParams['xtick.top'] = True
-plt.rcParams['ytick.right'] = False
-plt.rcParams['xtick.major.pad']='10'
-plt.rcParams['ytick.major.pad']='10'
 
 import glob
 files = glob.glob("*fits*")
